298ac8a0e8322a567432cb9135f6569d1363312c
Yab1 <yabilisanu@gmail.com>
2023-07-13 09:32:22 +0300

    Feature: Implement category object to organize and manage data by categories

---
+++ src/utils/categories.js
@@ -0,0 +1,22 @@
+const categories = [
+  { value: "Area", key: 0 },
+  { value: "Cafeteria", key: 1 },
+  { value: "Classroom", key: 2 },
+  { value: "Dormitory", key: 3 },
+  { value: "Gymnasium", key: 4 },
+  { value: "Hospital", key: 5 },
+  { value: "Laboratory", key: 6 },
+  { value: "Library", key: 7 },
+  { value: "Lounge", key: 8 },
+  { value: "Meeting Hall", key: 9 },
+  { value: "Office", key: 10 },
+  { value: "Others", key: 11 },
+  { value: "Parking Area", key: 12 },
+  { value: "Post Office", key: 13 },
+  { value: "Public Toilet", key: 14 },
+  { value: "Shop", key: 15 },
+  { value: "Shoe Shiner", key: 16 },
+  { value: "...View All...", key: 17 },
+];
+
+export default categories;
