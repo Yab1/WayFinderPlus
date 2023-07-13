8ebee540b03418c8696c9acdd31ad76ea9958290
Yab1 <yabilisanu@gmail.com>
2023-07-14 01:54:35 +0300

    Feature: Implement zoom-out function to enable wider view of the map

---
+++ src/functions/zoomOut.js
@@ -0,0 +1,9 @@
+export default function zoomOut(map) {
+  if (map) {
+    const currentZoom = map.getZoom();
+    if (currentZoom > 15.5) {
+      const newZoom = currentZoom - 0.5;
+      map.setZoom(newZoom);
+    }
+  }
+}
