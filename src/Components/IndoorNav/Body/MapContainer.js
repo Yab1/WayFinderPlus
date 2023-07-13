1dd9537a23d0e38b827f102e27a1f6436f267bef
Yab1 <yabilisanu@gmail.com>
2023-07-13 09:31:02 +0300

    Refactor: Update map container to improve performance and user experience

--- src/Components/IndoorNav/Body/MapContainer.js
+++ src/Components/IndoorNav/Body/MapContainer.js
@@ -1,28 +1,35 @@
 // React
-import { useEffect } from "react";
+import { useEffect, useContext, useRef } from "react";
+
+// Contexts
+import { MapMetaDataContext } from "../../../Context/mapMetaDataContext";
 
 // MapBox
 import mapboxgl from "mapbox-gl/dist/mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 // MUI Components
 import Card from "@mui/material/Card";
 
 const MapContainer = () => {
+  const { currentMap, handleMap } = useContext(MapMetaDataContext);
+
   useEffect(() => {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYWZyb2hhYmVzaGEiLCJhIjoiY2xnb3F0cDYzMGYzNjNlb2d2dXhtdzRqbSJ9.JW2kyDZjoOWoXVPG5Giw7g";
     const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/outdoors-v12",
-      center: [39.29039343419677, 8.563261132878523],
+      center: [currentMap[0].center.latitude, currentMap[0].center.longitude],
       zoom: 15.5,
       attributionControl: false,
     });
+    handleMap(map);
 
     return () => {
       map.remove();
     };
-  }, []);
+  }, [currentMap]);
 
   return (
     <Card
