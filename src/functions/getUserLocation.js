cfb3aee81c6a48565cd38f78b0af2dddd9e6ffd4
Yab1 <yabilisanu@gmail.com>
2023-07-14 01:52:12 +0300

    Feature: Add user location function to retrieve and display user's current location on the map

---
+++ src/functions/getUserLocation.js
@@ -0,0 +1,37 @@
+import mapboxgl from "mapbox-gl";
+
+export default function getUserLocation(map, setUserLocationState) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocationState([latitude, longitude]);
+        addMarkerToMap(map, latitude, longitude);
+      },
+      (error) => {
+        alert("Error getting user location:", error);
+      }
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+let userMarker = null;
+const addMarkerToMap = (map, latitude, longitude) => {
+  if (map) {
+    if (userMarker) {
+      userMarker.remove();
+    }
+    const markerElement = document.createElement("div");
+    markerElement.className = "userMarker";
+    userMarker = new mapboxgl.Marker({
+      element: markerElement,
+      anchor: "center",
+    })
+      .setLngLat([longitude, latitude])
+      .addTo(map);
+    map.setCenter([longitude, latitude]);
+    map.setZoom(17);
+  }
+};
