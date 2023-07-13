efc7154e42ed32843e399d1c7819f1bde507eb15
Yab1 <yabilisanu@gmail.com>
2023-07-13 09:28:24 +0300

    Enhancement: Include logo in the header for branding and visual identity

--- src/Components/IndoorNav/Header/Header.js
+++ src/Components/IndoorNav/Header/Header.js
@@ -1,5 +1,13 @@
+// assets
+import WayFinderLogoOnly from "../../../assets/WayfinderPlus Logo only.png.png";
+import WayFinderLogo from "../../../assets/WayfinderPlus Logo.png";
+
 const Header = () => {
-  return <header className="Header">header</header>;
+  return (
+    <header className="Header">
+      <img src={WayFinderLogoOnly} alt="" />
+    </header>
+  );
 };
 
 export default Header;
