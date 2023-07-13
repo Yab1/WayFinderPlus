e23b2ca0f4ac7e61bb0b1ccad67812c97d57e737
Yab1 <yabilisanu@gmail.com>
2023-07-13 09:28:49 +0300

    Feature: Enable category-based search functionality

--- src/Components/IndoorNav/SidebarLeft/SideBarLeft.js
+++ src/Components/IndoorNav/SidebarLeft/SideBarLeft.js
@@ -1,8 +1,32 @@
+// utils
+import categories from "../../../utils/categories";
+
 // MUI Components
-import Card from "@mui/material/Card";
+import Menu from "@mui/material/Menu";
+import Button from "@mui/material/Button";
+import { grey, deepOrange } from "@mui/material/colors";
 
 const SideBarLeft = () => {
-  return <section className="SideBarLeft">Left</section>;
+  return (
+    <section className="SideBarLeft">
+      {categories.map((category) => (
+        <Button
+          key={category.key}
+          sx={{
+            color: grey[900],
+            fontSize: "0.7em",
+            "&:hover": {
+              color: "#ff3d00",
+              fontSize: "1.2em",
+              fontWeight: "bold",
+            },
+          }}
+        >
+          {category.value}
+        </Button>
+      ))}
+    </section>
+  );
 };
 
 export default SideBarLeft;
