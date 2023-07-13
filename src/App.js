cdfdddbf5b76296a8c2f0f4ca09cffd0020c9bb8
Yab1 <yabilisanu@gmail.com>
2023-07-13 09:33:47 +0300

    Enhancement: Integrate two contexts (BuildingsContext and MapMetadataContext)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// Contexts
+import MapMetaDataContextProvider from "./Context/mapMetaDataContext";
+import BuildingsContextProvider from "./Context/buildingsContext";
+
 // Components
 import Header from "./Components/IndoorNav/Header/Header";
 import MapContainer from "./Components/IndoorNav/Body/MapContainer";
@@ -8,11 +12,15 @@ import Footer from "./Components/IndoorNav/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MapContainer />
-      <SideBarLeft />
-      <SideBarRight />
-      <Footer />
+      <MapMetaDataContextProvider>
+        <BuildingsContextProvider>
+          <Header />
+          <MapContainer />
+          <SideBarLeft />
+          <SideBarRight />
+          <Footer />
+        </BuildingsContextProvider>
+      </MapMetaDataContextProvider>
     </div>
   );
 }
