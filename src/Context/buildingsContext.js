65c2be1bd4229baa503bf556a3e9e8b9be9edd4b
Yab1 <yabilisanu@gmail.com>
2023-07-14 01:55:09 +0300

    Feature: Fetch buildings data from the database to populate the map with relevant information

---
+++ src/Context/buildingsContext.js
@@ -0,0 +1,40 @@
+import { createContext, useState, useEffect } from "react";
+import { app } from "../Services/Firebase/connection";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+
+export const BuildingsContext = createContext();
+
+export default function BuildingsContextProvider({ children }) {
+  const db = getFirestore(app);
+  const [buildingsData, setBuildingsData] = useState([]);
+  useEffect(() => {
+    const colRef = collection(
+      db,
+      "Locations",
+      "Adama Science And Technology",
+      "BuildingsData"
+    );
+    const queuedRef = query(colRef, orderBy("buildingNumber"));
+    const unsubscribe = onSnapshot(queuedRef, (snapshot) => {
+      let data = [];
+      snapshot.docs.forEach((doc) => {
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setBuildingsData(data);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const value = { buildingsData };
+  return (
+    <BuildingsContext.Provider value={value}>
+      {children}
+    </BuildingsContext.Provider>
+  );
+}
