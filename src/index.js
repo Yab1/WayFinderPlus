c67d476aea17e62d922b986d2b17241916cb843f
Yab1 <yabilisanu@gmail.com>
2023-07-16 09:20:12 +0300

    Enhancement: Import Mapbox Styler library for enhanced map styling capabilities

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// MapBox
+import "mapbox-gl/dist/mapbox-gl.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
