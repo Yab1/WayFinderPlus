3ad10b28d63862204f07aec4ead642a4eea731cd
Yab1 <yabilisanu@gmail.com>
2023-07-16 09:17:01 +0300

    Refactor: Remove unnecessary query for collection ordering since it is not required in the current implementation

--- src/Context/buildingsContext.js
+++ src/Context/buildingsContext.js
@@ -1,12 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { app } from "../Services/firebaseConnection";
-import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  query,
-  orderBy,
-} from "firebase/firestore";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 
 export const BuildingsContext = createContext();
 
@@ -20,8 +14,7 @@ export default function BuildingsContextProvider({ children }) {
       "Adama Science And Technology",
       "BuildingsData"
     );
-    const queuedRef = query(colRef, orderBy("buildingNumber"));
-    const unsubscribe = onSnapshot(queuedRef, (snapshot) => {
+    const unsubscribe = onSnapshot(colRef, (snapshot) => {
       let data = [];
       snapshot.docs.forEach((doc) => {
         data.push({ ...doc.data(), id: doc.id });
