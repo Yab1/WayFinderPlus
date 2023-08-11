0ec8a3225678d38aef23ca526031f7a4ee9d4e3c
Yab1 <yabilisanu@gmail.com>
2023-08-11 08:45:10 +0300

    Style: Used grid for styling.

--- src/Components/Outdoor/SidebarLeft/SideBarLeft.js
+++ src/Components/Outdoor/SidebarLeft/SideBarLeft.js
@@ -16,6 +16,14 @@ import checkAvailability from "../../../functions/checkAvailability";
 import Button from "@mui/material/Button";
 import { grey } from "@mui/material/colors";
 
+//Flamer Motion
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 1 } },
+};
+
 const SideBarLeft = () => {
   const { selectedCategory, setSelectedCategoryState } = useContext(
     StateControllerContext
@@ -28,35 +36,41 @@ const SideBarLeft = () => {
         const available = checkAvailability(buildingsData, category);
         if (!available) return null;
         return (
-          <Button
+          <motion.div
             key={category.key}
-            id={category.key}
-            sx={{
-              color:
-                selectedCategory && selectedCategory.key == category.key
-                  ? "#fb3262"
-                  : grey[900],
-              fontWeight:
-                selectedCategory && selectedCategory.key == category.key
-                  ? "bold"
-                  : "",
-              fontSize:
-                selectedCategory && selectedCategory.key == category.key
-                  ? "1.2em"
-                  : "0.7em",
-              "&:hover": {
-                color: "#fb3262",
-                fontSize: "1.2em",
-                fontWeight: "bold",
-              },
-            }}
-            onClick={() => {
-              setSelectedCategoryState(category);
-              resetView();
-            }}
+            variants={buttonVariants}
+            initial="hidden"
+            animate="visible"
           >
-            {category.value}
-          </Button>
+            <Button
+              id={category.key}
+              sx={{
+                color:
+                  selectedCategory && selectedCategory.key == category.key
+                    ? "#fb3262"
+                    : grey[900],
+                fontWeight:
+                  selectedCategory && selectedCategory.key == category.key
+                    ? "bold"
+                    : "",
+                fontSize:
+                  selectedCategory && selectedCategory.key == category.key
+                    ? "1.2em"
+                    : "0.7em",
+                "&:hover": {
+                  color: "#fb3262",
+                  fontSize: "1.2em",
+                  fontWeight: "bold",
+                },
+              }}
+              onClick={() => {
+                setSelectedCategoryState(category);
+                resetView();
+              }}
+            >
+              {category.value}
+            </Button>
+          </motion.div>
         );
       })}
     </section>
